c36fad3d49a5ef38c9901ee887de2b479686af4e
Ruan Ferreira <ruan@ruanf.co.za>
2019-07-09 11:15:16 +0200

    file and version updates

---
+++ src/browser_action/browser_action.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    var firebaseConfig = {
+        apiKey: "AIzaSyAWNm0cUo2Qnj67pYwBxhV6nL_20gF7k9I",
+        authDomain: "wcc-nwk.firebaseapp.com",
+        databaseURL: "https://wcc-nwk.firebaseio.com",
+        projectId: "wcc-nwk",
+        storageBucket: "",
+        messagingSenderId: "784468766076",
+        appId: "1:784468766076:web:249080f071d7e5c7"
+    };
+    firebase.initializeApp(firebaseConfig);
+});
